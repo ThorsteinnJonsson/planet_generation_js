928e757cf0dd886d18ba6d420d7ddea7e2a85fac
ThorsteinnJonsson <thorsteinnbaldvin@gmail.com>
2021-01-09 17:47:49 +0900

    Add working ThreeJS example to React app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import * as THREE from "three";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    // === THREE.JS CODE START ===
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild(renderer.domElement);
+    this.mount.appendChild(renderer.domElement);
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.03;
+      cube.rotation.y += 0.03;
+      renderer.render(scene, camera);
+    };
+    animate();
+    // === THREE.JS EXAMPLE CODE END ===
+  }
+
+  render() {
+    return (
+      <div ref={ref => (this.mount = ref)}/>
+    )
+  }
 }
+
 
 export default App;
