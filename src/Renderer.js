10ea371d6e992149b30bc70f9f51b115fa6391fd
ThorsteinnJonsson <thorsteinnbaldvin@gmail.com>
2021-01-09 20:58:02 +0900

    Refactor renderer code

--- src/Renderer.js
+++ src/Renderer.js
@@ -2,34 +2,82 @@ import React, { Component } from 'react'
 import * as THREE from "three";
 
 class Renderer extends Component {
-  
+
   componentDidMount() {
-    // === THREE.JS CODE START ===
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    this.mount.appendChild(renderer.domElement);
+
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    this.scene = new THREE.Scene();
+
+    // Add renderer
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setClearColor("#263238");
+    this.renderer.setSize(width, height);
+    this.mount.appendChild(this.renderer.domElement);
+
+    // Add camera
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    this.camera.position.z = 8;
+    this.camera.position.z = 5;
+
+    // Camera controls
+    // TODO
+
+    // Lights
+    var lights = [];
+    lights[0] = new THREE.PointLight(0x304ffe, 1, 0);
+    lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[0].position.set(0, 200, 0);
+    lights[1].position.set(100, 200, 100);
+    lights[2].position.set(-100, -200, -100);
+    this.scene.add(lights[0]);
+    this.scene.add(lights[1]);
+    this.scene.add(lights[2]);
+
+
+    // Model
     var geometry = new THREE.BoxGeometry(1, 1, 1);
     var material = new THREE.MeshBasicMaterial({ color: 0x00afff });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.005;
-      cube.rotation.y += 0.03;
-      renderer.render(scene, camera);
-    };
-    animate();
-    // === THREE.JS EXAMPLE CODE END ===
-  }
-  
+    this.model = new THREE.Mesh(geometry, material);
+    this.scene.add(this.model);
+
+    this.renderScene();
+    this.start();
+  };
+
+  start = () => {
+    if (!this.frameId) {
+      this.frameId = requestAnimationFrame(this.animate);
+    }
+  };
+
+  stop = () => {
+    cancelAnimationFrame(this.frameId);
+  };
+
+  animate = () => {
+    this.model.rotation.x += 0.005;
+    this.model.rotation.y += 0.03;
+
+    this.renderScene();
+    this.frameId = window.requestAnimationFrame(this.animate);
+  };
+
+  renderScene = () => {
+    if (this.renderer) {
+      this.renderer.render(this.scene, this.camera);
+    }
+  };
+
+
   render() {
     return (
-      <div ref={ref => (this.mount = ref)}/>
+      <div
+        ref={ref => (this.mount = ref)} />
     )
-  }
+  };
 }
 
 export default Renderer;
