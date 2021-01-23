5fc80bef9828195876d7ba297a4311183b11c8c3
ThorsteinnJonsson <thorsteinnbaldvin@gmail.com>
2021-01-23 16:35:37 +0900

    Make noise generation more customizable

--- src/Planet.js
+++ src/Planet.js
@@ -15,11 +15,33 @@ class Planet {
 
   }
 
+  getNoise = (noiseGenerator, x, y, z, numIter, scale, persistence, low, high) => {
+    let maxAmp = 0;
+    let amp = 1;
+    let freq = scale;
+    let noise = 0;
+
+    for (let iter = 0; iter < numIter; iter++) {
+      noise += noiseGenerator.simplex3(x * freq, y * freq, z * freq) * amp;
+      maxAmp += amp;
+      amp *= persistence;
+      freq *= 2;
+    }
+
+    noise /= maxAmp;
+    noise = noise * (high - low) / 2 + (high + low) / 2;
+    return noise;
+  };
 
   generateHeight = () => {
+    const numIter = 3;
+    const noiseScale = 2.0/this.radius;
+    const persistence = 0.25;
+    const minRad = 0.9;
+    const maxRad = 1.1;
+
     const posBuffer = this.icosphereMesh.geometry.getAttribute('position');
     const lenghtBuffer = posBuffer.array.length;
-    console.log(posBuffer);
     for (let i = 0; i < lenghtBuffer/3; i++) {
       // Get noise val
       const x = posBuffer.array[3*i]
@@ -27,10 +49,7 @@ class Planet {
       const z = posBuffer.array[3*i+2]
 
       // Generate noise
-      const noiseScale1 = 0.25 * this.radius;
-      const noiseScale2 = 0.5 * this.radius;
-      const noiseVal = this.noise.simplex3(x/noiseScale1, y/noiseScale1, z/noiseScale1) 
-                     * this.noise.simplex3(x/noiseScale2, y/noiseScale2, z/noiseScale2);
+      const radiusScalingFactor = this.getNoise(this.noise, x, y, z, numIter, noiseScale, persistence, minRad, maxRad);
 
       // To spherical
       const r = Math.sqrt(Math.pow(x,2) + Math.pow(y,2) + Math.pow(z,2));
@@ -38,14 +57,15 @@ class Planet {
       const phi = Math.atan2(y,x);
 
       // Add radius
-      const heightScale = 0.75;
-      const new_r = r * (1.0 + heightScale * noiseVal);
+      const heightScale = 1.0;
+      const new_r = r * (0.0 + heightScale * radiusScalingFactor);
 
-      // To cartesian
+      // Back to cartesian
       posBuffer.array[3*i] = new_r * Math.sin(theta) * Math.cos(phi);
       posBuffer.array[3*i+1] = new_r * Math.sin(theta) * Math.sin(phi);
       posBuffer.array[3*i+2] = new_r * Math.cos(theta);
     }
+
   };
   
 
